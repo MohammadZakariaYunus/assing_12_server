7f2dcf7ae5d9243955fd55bfb429c137901e8d41
MohammadZakariaYunus <mohammadzakariaabdullahyunus99@gmail.com>
2022-06-03 15:08:14 +0600

    All Done

--- index.js
+++ index.js
@@ -241,6 +241,7 @@ async function run() {
             const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' })
             res.send({ result, token });
         }
+
         )
     }
 
